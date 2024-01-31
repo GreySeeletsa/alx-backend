f1795a3b48acf2f3bd91c350209c36f17637107a
GreySeeletsa <seeletsagrey@gmail.com>
2024-01-31 11:02:43 +0000

    class FIFOCache that inherits from BaseCaching and is a caching system

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,32 +1,21 @@
 #!/usr/bin/env python3
-"""Module for task 1.
+"""Module for task 0.
 """
-from collections import OrderedDict
-
 from base_caching import BaseCaching
 
 
-class FIFOCache(BaseCaching):
-    """A caching system that inherits from BaseCaching and uses a FIFO
-    algorithm to discard items
+class BasicCache(BaseCaching):
+    """A caching system that inherits from BaseCaching and does not have a
+    limit on the num of items it can store
     """
-    def __init__(self):
-        """Init cache and calls parent class's init method
-        """
-        super().__init__()
-        self.cache_data = OrderedDict()
-
     def put(self, key, item):
         """Assigns the item value to the key in the dictionary self.cache_data
         """
         if key is None or item is None:
             return
         self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(False)
-            print("DISCARD:", first_key)
 
     def get(self, key):
-        """Retrieves an item by key
+        """Returns the value in self.cache_data linked to key
         """
         return self.cache_data.get(key, None)
